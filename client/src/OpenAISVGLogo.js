63f7208c10e7730fa4664293d711c6d9010a070d
KanakoKa <kanako@co-incidence.org>
2023-02-21 10:46:55 +0100

    Changement de la photo avec le lien interent Skameleon

--- client/src/OpenAISVGLogo.js
+++ client/src/OpenAISVGLogo.js
@@ -4,7 +4,8 @@ const OpenAISVGLogo = () => (
   <div class="w-1">
     <div style={{ width: "50px", height: "50px" }}>
       <img
-        src="https://i.imgur.com/EGGQJFm.jpg"
+        //src="https://i.imgur.com/EGGQJFm.jpg"
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.skema-bs.fr%2Fskameleon&psig=AOvVaw2tX-RQ0Fev3fKDig_yo4hO&ust=1677058214686000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKjEn-ympv0CFQAAAAAdAAAAABAI"
         style={{ width: "100%", height: "100%", borderRadius: "50%" }}
       ></img>
     </div>
